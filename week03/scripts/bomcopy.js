20e637322db8c9bb865ca25ce764a108b6ef1e49
ereksonbrenda <ere18002@byui.edu>
2023-09-30 20:02:15 -0600

    copied bom to week3

--- week03/scripts/bomcopy.js
+++ week03/scripts/bomcopy.js
@@ -3,23 +3,49 @@ const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
 button.addEventListener('click', function () {
-    if (input.value !== '') {
-        const li = document.createElement('li');
-        const deleteButton = document.createElement('button');
-        li.textContent = input.value;
-        deleteButton.textContent = '❌';
-        li.appendChild(deleteButton);
-        list.appendChild(li);
-
-        deleteButton.addEventListener('click', function (e) {
-            list.removeChild(li);
-            input.focus();
-        });
-
+    if (input.value !== '') 
+    {   displayList(input.value);
+        chaptersArray.push(input.value);
+        setChapterList();
         input.value = '';
         input.focus();
-
     }
-    
+
+    });
+
+let chaptersArray = getChaptersList() || [];
+chaptersArray.array.forEach(chapter => {
+    displayList(chapter);
+
     
 });
+function displayList(item) {
+  let li = document.createElement('li');
+  let deletebutton = document.createElement('button');
+  li.textContent = item; 
+  deletebutton.textContent = '❌';
+  deletebutton.classList.add('delete'); 
+  li.append(deletebutton);
+  list.append(li);
+  deletebutton.addEventListener('click', function () {
+    list.removeChild(li);
+    deleteChapter(li.textContent); 
+    input.focus(); 
+  });
+  console.log('I like to copy code instead of typing it out myself and trying to understand it.');
+}
+function setChapterList() {
+    localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+  }
+  function getChapterList() {
+    return JSON.parse(localStorage.getItem('myFavBOMList'));
+  }
+  chapter = chapter.slice(0, chapter.length - 1);
+  
+  chaptersArray = chaptersArray.filter((item) => item !== chapter);
+  
+  function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter(item => item !== chapter);
+    setChapterList();
+  }   
