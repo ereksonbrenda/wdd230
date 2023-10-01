475446006a964017a2df38dc0f50b795a74f7c62
ereksonbrenda <ere18002@byui.edu>
2023-09-30 18:35:06 -0600

    worked on js

--- scripts/localstorage.js
+++ scripts/localstorage.js
@@ -1,8 +1,8 @@
 const visitsDisplay = document.querySelector(".visits");
 let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
-if(numvisits !== 0) {
+if(numVisits !== 0) {
     visitsDisplay.textContent = `This is your first time visiting. Welcome.`;
 }
-numvisits++;
+numVisits++;
 
 localStorage.setItem("numVisits-ls", numVisits);
