3563d2cc8e358ea26b9781b04848dfb3c69af37e
ereksonbrenda <73319895+ereksonbrenda@users.noreply.github.com>
2023-10-07 18:55:45 -0600

    designed new join page

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -28,3 +28,20 @@ if (visitCount) {
 };
 
 window.onload = updateCount;
+
+var today = new Date();
+var dd = (today.getDate()).toString();
+var mm = (today.getMonth()+1).toString(); 
+var yy = (today.getFullYear()-2000).toString();
+if(dd<10) 
+{
+    dd='0'+dd;
+} 
+if(mm<10) 
+{
+    mm='0'+mm;
+} 
+today = yy+mm+dd;
+
+
+document.getElementById('timeStamp').value= today ; 
